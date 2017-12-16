b3f892086c298176548cb57c3240e060cd01354c
IAMelentsov <imelentsov@gmail.com>
2017-12-16 16:42:19 +0500

    gitignore

---
+++ hakaton.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+Created on 2017-12-16 15:26
+@summary: 
+@author: i.melentsov
+'''
+
+class Message_provider(object):
+	"""docstring for Message_provider"""
+	def __init__(self, arg):
+		super(Message_provider, self).__init__()
+		self.arg = arg
+
+	def get_message():
+		pass
+		
