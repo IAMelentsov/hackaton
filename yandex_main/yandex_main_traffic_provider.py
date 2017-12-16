0706b8f29a1efd7bcf56b8c6169751dfec72f6a0
IAMelentsov <imelentsov@gmail.com>
2017-12-16 19:58:47 +0500

    refactor + yandex traffic impoved

--- yandex_main/yandex_main_traffic_provider.py
+++ yandex_main/yandex_main_traffic_provider.py
@@ -5,9 +5,10 @@ Created on 2017-12-16 18:21
 @summary: 
 @author: i.melentsov
 '''
+if __name__ == '__main__':
+    import sys
+    sys.path = ['../'] + sys.path
 
-import sys
-sys.path = ['../'] + sys.path
 from yandex_main_loader import YandexMainMessageProvider
 
 class YandexMainTrafficProvider(YandexMainMessageProvider):
@@ -16,15 +17,14 @@ class YandexMainTrafficProvider(YandexMainMessageProvider):
         super(YandexMainMessageProvider, self).__init__()
 
     def get_message(self):
-        soup = self.get_soup()
-        traffic_ball = soup.findAll("a", { "data-statlog" : "traffic.ball" })[0].text
-        traffic_more = soup.findAll("a", { "data-statlog" : "traffic.more" })[0].text
+        soup = self.get_soup().find(id="wd-_traffic")
+        traffic_ball = soup.find("a", { "data-statlog" : "traffic.ball" }).text
+        traffic_more = soup.find("a", { "data-statlog" : "traffic.more" }).text
         return 'По данным яндекса, пробки - {}. {}.'.format(
                 traffic_ball,
                 traffic_more
             )
 
-   
 if __name__ == '__main__':
     ymmp = YandexMainTrafficProvider()
     print(ymmp.get_message())
