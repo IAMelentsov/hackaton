169b83a07645b836ef329160af663d989992ef69
IAMelentsov <imelentsov@gmail.com>
2017-12-16 20:51:32 +0500

    main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+Created on 2017-12-16 20:44
+@summary: 
+@author: i.melentsov
+'''
+
+from weather.wether_provider import WeatherProvider
+from yandex_main.yandex_main_afisha_message_provider import YandexMainAfishaMessageProvider
+from yandex_main.yandex_main_traffic_provider import YandexMainTrafficProvider
+from nameday.nameday_provider import NamedayProvider
+
+if __name__ == '__main__':
+	main()
